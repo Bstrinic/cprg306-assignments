5c71ffb3dd21e2f22a6b78b54229e5718651197e
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-11 10:40:03 -0600

    Created header for landing page and added the links without the connection to folders

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>CPRG 306 - Web Development 2 - Assignments</h1>
+
+      <ul>
+        <li>
+          <Link href="">Week 2 Assignment</Link>
+          <Link href="">Week 3 Assignment</Link>
+          <Link href="">Week 4 Assignment</Link>
+          <Link href="">Week 5 Assignment</Link>
+          <Link href="">Week 6 Assignment</Link>
+          <Link href="">Week 7 Assignment</Link>
+          <Link href="">Week 8 Assignment</Link>
+          <Link href="">Week 9 Assignment</Link>
+          <Link href="">Week 10 Assignment</Link>
+        </li>
+      </ul>
     </main>
   );
 }
