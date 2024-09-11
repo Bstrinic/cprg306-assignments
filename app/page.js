a9038770781f812e04e1f5cc420db475be5dbd57
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-11 10:52:41 -0600

    Completed linking on landing page and completed week 2 assignment

--- app/page.js
+++ app/page.js
@@ -7,15 +7,31 @@ export default function Home() {
 
       <ul>
         <li>
-          <Link href="">Week 2 Assignment</Link>
-          <Link href="">Week 3 Assignment</Link>
-          <Link href="">Week 4 Assignment</Link>
-          <Link href="">Week 5 Assignment</Link>
-          <Link href="">Week 6 Assignment</Link>
-          <Link href="">Week 7 Assignment</Link>
-          <Link href="">Week 8 Assignment</Link>
-          <Link href="">Week 9 Assignment</Link>
-          <Link href="">Week 10 Assignment</Link>
+          <Link href="./week-2/">Week 2 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-3/">Week 3 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-4/">Week 4 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-5">Week 5 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-6/">Week 6 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-7/">Week 7 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-8/">Week 8 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-9/">Week 9 Assignment</Link>
+        </li>
+        <li>
+          <Link href="./week-10/">Week 10 Assignment</Link>
         </li>
       </ul>
     </main>
