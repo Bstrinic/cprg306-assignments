5002096933db378ffb90fd456fbcb5053096ac57
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-11 10:30:30 -0600

    Created studentinfo page, added a component with <p> element and a <Link> element. Proceded to link it with page.js to display the information

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>Bogdan Strinic</p>
+            <Link href="https://github.com/Bstrinic/cprg306-assignments">https://github.com</Link>
+        </main>
+    )
+}
