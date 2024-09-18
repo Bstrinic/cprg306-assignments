df3251494dc7076f067f416bfced085cd023f4e4
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-18 10:33:24 -0600

    Created item component and accepted required props

---
+++ app/week-3/item.js
@@ -0,0 +1,11 @@
+
+
+export default function Item({ name, quantity, category }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{quantity}</p>
+      <p>{category}</p>
+    </div>
+  );
+}
