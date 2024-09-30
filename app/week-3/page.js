fcfe43c301b9bafb1b7a76c1e53c31d7e377f5fc
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-30 17:32:47 -0600

    corrected code

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import ItemList from "./item-list";
 
@@ -6,9 +6,9 @@ export default function Page() {
   return (
     <main className="bg-cyan-400 flex justify-center">
       <div>
-      <HelmetProvider>
-        <title>Shopping List</title>
-      </HelmetProvider>
+        <HelmetProvider>
+          <title>Shopping List</title>
+        </HelmetProvider>
         <h1 className="justify-center flex text-4xl font-bold text-black">
           Shopping List
         </h1>
