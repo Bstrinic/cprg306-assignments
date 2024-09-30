9990789ba0c59fe954b370944f52e42a16ca6174
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-30 17:30:47 -0600

    last couple changes for styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -6,8 +6,8 @@ export default function Item({ name, quantity, category }) {
     <div className="bg-pink-500 border border-black rounded outline p-5 my-5 mx-10
     w-80 flex: 1 justify-center text-black font-bold">
       <h2 className="text-xl">{name}</h2>
-      <p>{quantity}</p>
-      <p>{category}</p>
+      <p>Buy {quantity} in {category}</p>
+      <p></p>
     </div>
   );
 }
