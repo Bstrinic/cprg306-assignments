a4971a9d1d8324fa3ff87faf22156747b2081bcf
Bstrinic <BogdanStrinic003@gmail.com>
2024-09-30 17:38:59 -0600

    Realized that I made a mistake with class instead of className and corrected it

--- app/page.js
+++ app/page.js
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main class="flex items-center flex-col justify-between p-24">
-      <div class="flex justify-center items-center">
-        <h1 class="font-bold text-4xl mb-5">CPRG 306 - Web Development 2 - Assignments</h1>
+    <main className="flex items-center flex-col justify-between p-24">
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold text-4xl mb-5">CPRG 306 - Web Development 2 - Assignments</h1>
       </div>
 
       <ul>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-2/">Week 2 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-3/">Week 3 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-4/">Week 4 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-5">Week 5 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-6/">Week 6 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-7/">Week 7 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-8/">Week 8 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-9/">Week 9 Assignment</Link></li>
-        <li class="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-10/">Week 10 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-2/">Week 2 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-3/">Week 3 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-4/">Week 4 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-5">Week 5 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-6/">Week 6 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-7/">Week 7 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-8/">Week 8 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-9/">Week 9 Assignment</Link></li>
+        <li className="hover:text-green-500 hover:underline mb-2 font-bold"><Link href="./week-10/">Week 10 Assignment</Link></li>
       </ul>
     </main>
   );
