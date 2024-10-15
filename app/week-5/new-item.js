661009d8671b01beb4bbf2fa09931cddedb76559
Bstrinic <BogdanStrinic003@gmail.com>
2024-10-15 17:17:06 -0600

    Created item name input and produce selection dropdown

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -3,13 +3,44 @@
 import { useState } from "react";
 
 export default function NewItem() {
-  // initializing state variables
+  
+  // quantity variable 
   const [quantity, setQuantity] = useState(1);
-
   // name variable
   const [name, setName] = useState("");
   // Category variable
   const [category, setCategory] = useState("Produce")
+
+  // Creating form submission handler
+  const handleSubmit = (event) => {
+
+    // Preventing default submission behavior
+    event.preventDefault();
+
+    let item = {
+      iName: itemName,
+      tQuantity: totalQuantity,
+      iCategory: itemCategory 
+    }
+
+    console.log(item);
+
+    alert(`
+      Item Name: ${item.iName}
+      Quantity: ${item.tQuantity}
+      Category: ${item.iCategory}
+      `)
+
+      setName("");
+      setQuantity("");
+      setCategory("");
+  };
+
+
+
+
+
+
 
   // Button disabled
   const incrementDisabled = quantity >= 20;
@@ -48,28 +79,63 @@ export default function NewItem() {
   //   }
 
   return (
-    <div className="bg-slate-100 flex m-5 w-1/2 justify-center p-3">
-      <div className="flex justify-between  ">
-        <p className="text-black m-3 p-2 font-bold text-lg">{quantity}</p>
-        <div className="flex m-3 p-2">
-          <button
-            className={`${buttonStyles} ${
-              decrementDisabled ? "bg-gray-500" : ""
-            }`}
-            onClick={decrement}
-          >
-            -
-          </button>
-          <button
-            className={`${buttonStyles} ${
-              incrementDisabled ? "bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-900" : ""
-            }`}
-            onClick={increment}
-          >
-            +
-          </button>
-        </div>
+    <form>
+      <div>
+        <label className="w-40 border-2 border-gray-300 rounded-lg font-sans">
+          <input defaultValue="Item Name"></input>
+        </label>
       </div>
-    </div>
+      <div>
+        <label>
+          <select>
+            <option selected value="">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Bakery">Bakery</option>
+            <option value="Meat">Meat</option>
+            <option value="Frozen Foods">Frozen Foods</option>
+            <option value="Canned Goods">Canned Goods</option>
+            <option value="Dry Goods">Dry Goods</option>
+            <option value="Beverages">Beverages</option>
+            <option value="Snacks">Snacks</option>
+            <option value="Household">Household</option>
+            <option value="Other">Other</option>
+          </select>
+        </label>
+      </div>
+    </form>
+
+
+
+
+
+
+
+
+
+
+
+    // <div className="bg-slate-100 flex m-5 w-1/2 justify-center p-3">
+    //   <div className="flex justify-between  ">
+    //     <p className="text-black m-3 p-2 font-bold text-lg">{quantity}</p>
+    //     <div className="flex m-3 p-2">
+    //       <button
+    //         className={`${buttonStyles} ${
+    //           decrementDisabled ? "bg-gray-500" : ""
+    //         }`}
+    //         onClick={decrement}
+    //       >
+    //         -
+    //       </button>
+    //       <button
+    //         className={`${buttonStyles} ${
+    //           incrementDisabled ? "bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-900" : ""
+    //         }`}
+    //         onClick={increment}
+    //       >
+    //         +
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
