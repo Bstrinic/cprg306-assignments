955aee0db7d1d5cac8374a03d04cb2f0814dd971
Bstrinic <BogdanStrinic003@gmail.com>
2024-10-16 13:58:20 -0600

    Added existing folders and created items.json file

---
+++ app/week-6/page.js
@@ -0,0 +1,19 @@
+"use client";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="bg-cyan-400 flex justify-center">
+      <div>
+        <HelmetProvider>
+          <title>Shopping List</title>
+        </HelmetProvider>
+        <h1 className="justify-center flex text-4xl font-bold text-black">
+          Shopping List
+        </h1>
+        <ItemList />
+      </div>
+    </main>
+  );
+}
