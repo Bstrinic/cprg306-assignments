e8d9c451caf44a75e1b186c89e582d2962190af9
Bstrinic <BogdanStrinic003@gmail.com>
2024-10-15 20:39:57 -0600

    Corrected styling and logic of assignment, need to fix button style

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -10,23 +10,27 @@ export default function NewItem() {
   // Category variable
   const [itemCategory, setItemCategory] = useState("Produce");
 
+  const handleItemNameChange = (event) => setItemName(event.target.value);
+  const handleQuantityChange = (event) => setItemQuantity(event.target.value);
+  const handleCategoryChange = (event) => setItemCategory(event.target.value)
+
   // Creating form submission handler
   const handleSubmit = (event) => {
     // Preventing default submission behavior
     event.preventDefault();
 
     let item = {
-      iName: itemName,
-      tQuantity: totalQuantity,
-      iCategory: itemCategory,
+      itemName: itemName,
+      totalQuantity: totalQuantity,
+      itemCategory: itemCategory,
     };
 
     console.log(item);
 
     alert(`
-      Item Name: ${item.iName}
-      Quantity: ${item.tQuantity}
-      Category: ${item.iCategory}
+      Item Name: ${item.itemName}
+      Quantity: ${item.totalQuantity}
+      Category: ${item.itemCategory}
       `);
 
     setItemName("");
@@ -35,7 +39,6 @@ export default function NewItem() {
   };
 
   // Button disabled
-  const incrementDisabled = totalQuantity >= 20;
   const decrementDisabled = totalQuantity <= 1;
 
   // Button enabled
@@ -44,17 +47,17 @@ export default function NewItem() {
 
   // Function to increment counter
   const increment = () => {
-    let currentQuantity = quantity;
-    if (quantity <= 19) {
-      setQuantity(currentQuantity + 1);
+    let currentQuantity = totalQuantity;
+    if (totalQuantity >= 1) {
+      setItemQuantity(totalQuantity + 1);
     }
   };
 
   // Function to decrement counter
   const decrement = () => {
-    let currentQuantity = quantity;
-    if (quantity >= 2) {
-      setQuantity(currentQuantity - 1);
+    let currentQuantity = totalQuantity;
+    if (totalQuantity >= 2) {
+      setItemQuantity(totalQuantity - 1);
     }
   };
 
@@ -63,53 +66,58 @@ export default function NewItem() {
   //       "w-8 bg-gray-500 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75";
   //   }
 
-  //   if (quantity <= 1) {
-  //     buttonStyles =
-  //       "w-8 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75";
-  //   }
+    if (totalQuantity <= 1) {
+      buttonStyles =
+        "w-8 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75";
+    }
 
   return (
     <form className="p-2 m-4 bg-slate-900 text-black max-w-sm w-full">
+
       <div className="mb-3">
         <label className="flex justify-center">
           <input
-            className="w-full border-2 border-gray-300 rounded-lg font-sans text-black "
-            defaultValue="Item Name"
+            type="text"
+            onChange={handleItemNameChange}
+            className="w-full border-2 border-gray-300 rounded-lg font-sans text-black p-2 m-2 "
+            placeholder="Item Name"
           ></input>
         </label>
       </div>
-      <div className=" flex justify-between p-2 mb-1">
-        <div className="flex p-2 mt-1 rounded-md bg-white">
+
+
+
+
+
+      <div className="flex justify-between ">
+        <div className="flex p-2 m-3 rounded-md bg-white w-1/2">
           <p className="text-black m-3 p-2 font-bold text-lg">
             {totalQuantity}
           </p>
           <div className="flex m-3 p-2">
-            <button
-              className={`${buttonStyles} ${
-                decrementDisabled ? "bg-gray-500" : ""
-              }`}
+            <button 
+              type="button"
+              onChange={handleQuantityChange}
+              className={buttonStyles}
               onClick={decrement}
             >
               -
             </button>
             <button
-              className={`${buttonStyles} ${
-                incrementDisabled
-                  ? "bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                  : ""
-              }`}
+              type="button"
+              onChange={handleQuantityChange}
+              className={buttonStyles}
               onClick={increment}
             >
               +
             </button>
           </div>
         </div>
-
-        <div className="flex justify-center">
+        <div className="flex p-2 m-3 rounded-md bg-white w-1/2">
           <label className="justify-center flex">
-            <select
+            <select 
               defaultValue="Produce"
-              onChange={(event) => setItemCategory(event.target.value)}
+              onChange={handleCategoryChange}
               className="border-2 border-gray-300 rounded-lg font-sans text-black justify-center flex "
             >
               <option value="produce">Produce</option>
@@ -127,14 +135,17 @@ export default function NewItem() {
           </label>
         </div>
       </div>
-      <div className="m-3 flex justify-center">
+
+      <div className="p-2 m-2 flex justify-center ">
         <button
-          className="w-full bg-green-600 border-2 border-green-800 rounded-lg text-black"
+          type="submit"
+          className="w-full border-2 bg-green-500 border-green-950 rounded-lg font-sans text-black p-2 m-2 "
           onClick={handleSubmit}
         >
           +
         </button>
       </div>
+
     </form>
 
     // <div className="bg-slate-100 flex m-5 w-1/2 justify-center p-3">
