6c7aab32deafba1ed7a305c4316cbb5e9c14ea5d
Bstrinic <BogdanStrinic003@gmail.com>
2024-10-22 18:49:27 -0600

    final touches

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -5,10 +5,10 @@ import { useState } from "react";
 import items from "./items.json";
 
 export default function ItemList() {
-
   // Creating useState for sortBy
   const [sortBy, setSortBy] = useState("name");
 
+  // Sorting for name and category
   items.sort((a, b) => {
     if (sortBy === "name") {
       return a.name.localeCompare(b.name);
@@ -19,20 +19,19 @@ export default function ItemList() {
     }
   });
 
-  // if (setSortBy === "name") {
-  //   buttonStyles = "bg-pink-500 p-1 m-2 w-28 border-4 border-teal-800 shadow-md hover:bg-teal-900 focus:outline-none";
-  // } else if (setSortBy === "category") {
-  //   buttonStyles = "bg-pink-500 p-1 m-2 w-28 border-4 border-teal-800 shadow-md hover:bg-teal-900 focus:outline-none";
-  // }
-
   // Defining button styles
   const buttonStyles =
-    "p-1 m-2 w-28 border-4 border-orange-500 rounded shadow-md hover: focus:outline-none ";
+    "p-1 m-2 w-28 border-4 border-orange-500 rounded shadow-md focus:outline-none ";
 
-    // Ternary Operator - Syntax: condition ? <expression if true> : <expression if false>
-  const nameButtonStyle = sortBy === "name" ? buttonStyles + "bg-orange-500" : buttonStyles + "bg-orange-800";
-  const categoryButtonStyle = sortBy === "category" ? buttonStyles + "bg-orange-500" : buttonStyles + "bg-orange-800";
-
+  // Ternary Operator - Syntax: condition ? <expression if true> : <expression if false>
+  const nameButtonStyle =
+    sortBy === "name"
+      ? buttonStyles + "bg-orange-500"
+      : buttonStyles + "bg-orange-800";
+  const categoryButtonStyle =
+    sortBy === "category"
+      ? buttonStyles + "bg-orange-500"
+      : buttonStyles + "bg-orange-800";
 
   return (
     <div className="m-5">
@@ -43,7 +42,10 @@ export default function ItemList() {
         <button className={nameButtonStyle} onClick={() => setSortBy("name")}>
           Name
         </button>
-        <button className={categoryButtonStyle} onClick={() => setSortBy("category")}>
+        <button
+          className={categoryButtonStyle}
+          onClick={() => setSortBy("category")}
+        >
           Category
         </button>
       </div>
@@ -58,15 +60,6 @@ export default function ItemList() {
               Buy {item.quantity} in {item.category}
             </p>
           </div>
-
-          // <li
-          //   className="bg-pink-500 border border-black rounded outline p-5 my-5 mx-10
-          //   w-80 flex: 1 justify-center text-black font-bold"
-          //   key={item.id}>
-          //   {item.name}
-          //   {item.quantity}
-          //   {item.category}
-          // </li>
         ))}
       </ul>
     </div>
