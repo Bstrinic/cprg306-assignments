510645620a98e64e0077261a835f121498fd04f6
Bstrinic <BogdanStrinic003@gmail.com>
2024-11-05 20:13:11 -0700

    Creating return to render the component

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -1,24 +1,48 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-// Creating an fetch function for API 
+// Creating an fetch function for API
 const fetchMealIdeas = async (ingredient) => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-    const data = await response.json();
-    return data.meals;
-}
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+  );
+  const data = await response.json();
+  return data.meals;
+};
 
-export default function MealIdeas([ ingredient ]) {
+export default function MealIdeas([ingredient]) {
+  // Defining state variable for meals
+  const [meals, setMeals] = useState([]);
 
-    // Defining state variable for meals
-    const [meals, setMeals] = useState([]);
+  // Defining load function
+  const loadMealIdeas = async () => {
+    // Storing fetchMealIdeas in data variable
+    const data = await fetchMealIdeas(ingredient);
+    // Setting the state
+    setMeals(data);
+  };
 
-    // Defining load function 
-    const loadMealItems = async () => {
-        // Storing fetchMealIdeas in data variable
-        const data = await fetchMealIdeas(ingredient);
-        // Setting the state
-        setMeals(data);
-    }
+  // Creating a useEffect hook
+  useEffect(() => {
+    // Calling function
+    loadMealIdeas();
+    // React to change
+  }, [ingredient]);
+
+  return(
+    <div>
+        <header>
+            <h1>Meal Ideas</h1>
+        </header>
+        <p>Select an item to see meal ideas</p>
+        <ul>
+            {meals.map((meals) => (
+                <li key={meals.idMeal}>
+                    {meals.strMeal}
+                </li>
+            ))}
+        </ul>
+    </div>
+  );
 }
