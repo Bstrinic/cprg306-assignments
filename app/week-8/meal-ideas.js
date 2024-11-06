f4732f903d9a734a17837196895864d3b2071a9a
Bstrinic <BogdanStrinic003@gmail.com>
2024-11-05 19:41:47 -0700

    Defined API fetching function

---
+++ app/week-8/meal-ideas.js
@@ -0,0 +1,17 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+// Creating an fetch function for API 
+const fetchMealIdeas = async (ingredient) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+    const data = await response.json();
+    return data.meals;
+}
+
+export default function MealIdeas([ ingredient ]) {
+
+    // Defining state variable for meals
+    const [meals, setMeals] = useState([]);
+
+}
